1daa247caa929b363ee414fb21fe0e01ecc303f0
sikim <suchae7@naver.com>
2026-05-07 09:49:42 +0900

    cors 오류 수정:

--- law-rag-engine/app/main.py
+++ law-rag-engine/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.api.documents import router as documents_router
 
@@ -8,6 +9,16 @@ app = FastAPI (
     version = "0.1.1",
 )
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 app.include_router(documents_router)
 
 # JAVA
